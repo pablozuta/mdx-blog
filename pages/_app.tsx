1249228dd9f927d454b5e5336de363077d779a30
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-01-28 00:50:30 -0400

    se agrega post javascript falsy values

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,19 @@ import Link from "next/link";
 import Head from "next/head";
 import "@/styles/globals.css";
 import Image from "next/image";
-import twitter from "../public/twitter.svg";
-import github from "../public/github.svg";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   
   return (
     <div className="container">
       <Head>
-        <title>Blog && Notes</title>
+        <title>Blog & Notes</title>
       </Head>
       <nav className="barra-navegacion">
         <h1 className="home blog-title">
          
-          <Link href="/">BLOG && NOTES</Link>
+          <Link href="/">BLOG & NOTES</Link>
         </h1>
 
       
